d6ec412e1c1d021d042d3e85697f7d4454e6ab26
William Mittard <billymittard@ymail.com>
2020-04-18 19:49:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,9 +21,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
 end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
+def add_supply_to_winter_holidays(christmas, new_years)
   holiday_hash[:winter].each do |holiday_hash, champagne|
-    champagne << supply
+    champagne << new_years
   end 
 end
 
