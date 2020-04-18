1a2d48e933ed44825e4739582135cfad0bdfe2a9
William Mittard <billymittard@ymail.com>
2020-04-18 19:46:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,8 +22,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter].each do |holiday_hash, new_supply|
-    new_supply << supply
+  holiday_hash[:winter].each do |holiday_hash, champagne|
+    champagne << supply
   end 
 end
 
